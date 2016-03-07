25d196ebb677a4aa45934a43bc19d4a62073aa6f
Michael Yockey <mike@heysparkbox.com>
2016-03-07 17:19:16 -0500

    fix: Failing just a little bit better
    Resolves #24

--- lib/mina-circle/helpers.rb
+++ lib/mina-circle/helpers.rb
@@ -1,15 +1,7 @@
 module MinaCircle
   module Helpers
-    def build_url
-      latest_build = circle_ci.builds.first
-      circle_ci.artifacts(latest_build).select { |artifact|
-        artifact.filename == circle_artifact
-      }.first.url
-    end
-
     def circle_ci
       @circle_ci ||= CircleCI.new circle_user, circle_project, branch
     end
   end
 end
-
