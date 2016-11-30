40c13235cf87367a563c168b4358ad3e35fe252b
Michael Yockey <mike@heysparkbox.com>
2016-11-30 14:45:42 -0500

    test: Testing artifact url validity

--- test/mina-circle/circle-ci/test_project.rb
+++ test/mina-circle/circle-ci/test_project.rb
@@ -3,6 +3,6 @@ require './test/helper'
 class TestProject < Minitest::Test
   def test_api_path
     project = CircleCI::Project.new organization: 'foo', name: 'bar', branch: 'master'
-    assert_equal 'github/foo/bar', project.api_path
+    assert_equal 'project/github/foo/bar', project.api_path
   end
 end
