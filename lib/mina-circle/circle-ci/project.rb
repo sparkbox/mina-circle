b11e9caf17ccecdec25d21d93682564a3dc0c8c4
Ryan Cromwell <ryan@heysparkbox.com>
2018-03-02 00:22:36 -0500

    fix (Project): Rely on `tree` scheme for specifying branches
    Per CircleCI documentation, branch is specified on the API path using
    `tree/:branch`: https://circleci.com/docs/api/v1-reference/#recent-builds-project-branch

--- lib/mina-circle/circle-ci/project.rb
+++ lib/mina-circle/circle-ci/project.rb
@@ -13,19 +13,33 @@ class CircleCI::Project
   end
 
   def api_path
+    api_path_parts.join('/')
+  end
+
+  def build_path
+    parts = api_path_parts + ['tree', real_branch]
+      
+    parts.compact.join('/')
+  end
+
+  private
+
+  def api_path_parts
     [
       'project',
       vcs_type,
       organization,
       name
-    ].join('/')
+    ]
   end
 
-  private
+  def real_branch
+    branch || 'master'
+  end
 
   def fetch_artifacts
     # To support Circle 2.0
-    build_info = CircleCI::Client.instance.get "#{api_path}", filter: 'successful', branch: branch ? branch : 'master', has_artifacts: true
+    build_info = CircleCI::Client.instance.get "#{build_path}", filter: 'successful', has_artifacts: true
 
     build_num = 'latest' # circle version 1.0
 
@@ -35,7 +49,7 @@ class CircleCI::Project
 
     puts "Using Build: #{build_num}"
 
-    artifact_hashes = CircleCI::Client.instance.get "#{api_path}/#{build_num}/artifacts", filter: 'successful', branch: branch ? branch : 'master'
+    artifact_hashes = CircleCI::Client.instance.get "#{api_path}/#{build_num}/artifacts", filter: 'successful', branch: real_branch
     artifact_hashes.collect { |artifact_hash|
       CircleCI::Artifact.new artifact_hash
     }
--- test/mina-circle/circle-ci/test_project.rb
+++ test/mina-circle/circle-ci/test_project.rb
@@ -5,4 +5,9 @@ class TestProject < Minitest::Test
     project = CircleCI::Project.new organization: 'foo', name: 'bar', branch: 'master'
     assert_equal 'project/github/foo/bar', project.api_path
   end
+
+  def test_build_path
+    project = CircleCI::Project.new organization: 'foo', name: 'bar', branch: 'master'
+    assert_equal 'project/github/foo/bar/tree/master', project.build_path
+  end
 end
